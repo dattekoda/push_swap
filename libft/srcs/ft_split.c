273f232b992fb612ad86dde84ba9a5d966d21904
Kodai Hanadate <khanadat@c4r1s8.42tokyo.jp>
2025-06-30 09:17:24 +0900

    update split

--- libft/srcs/ft_split.c
+++ libft/srcs/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khanadat <khanadat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 08:57:38 by khanadat          #+#    #+#             */
-/*   Updated: 2025/06/21 04:18:11 by khanadat         ###   ########.fr       */
+/*   Updated: 2025/06/28 15:08:54 by khanadat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	**ft_split(const char *s, char c)
 	char	**split;
 	char	**_split;
 
-	split = (char **)ft_calloc(count_words(s, c), sizeof(char *));
+	split = (char **)ft_calloc(count_words(s, c) + 1, sizeof(char *));
 	_split = split;
 	while (split && *s)
 	{
